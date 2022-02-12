962ea841e7630f63a462bf55a46c74b844b4d324
kimjjunho <kimjjun1007@gmail.com>
2022-02-13 03:09:59 +0900

    Change::Linear to Constraint

--- function/SlidingLayout/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ function/SlidingLayout/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +13,13 @@ class MainActivity : AppCompatActivity() {
 
         val mainFrame : SlidingUpPanelLayout = findViewById(R.id.mainFrame)
         val button : Button = findViewById(R.id.button)
+        val btn : Button = findViewById(R.id.btn)
 
         button.setOnClickListener {
             mainFrame.panelState = SlidingUpPanelLayout.PanelState.ANCHORED
+        }
+        btn.setOnClickListener {
+            startActivity(Intent(this, SubActivity::class.java))
         }
     }
 }
