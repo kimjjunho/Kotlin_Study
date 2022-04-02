00690ded0ed7a663da8672b8437da375c8991a97
kimjjunho <kimjjun1007@gmail.com>
2022-04-02 23:41:51 +0900

    try::FCM

--- function/FCM/FunctionFCM/app/src/main/java/com/example/functionfcm/MainActivity.kt
+++ function/FCM/FunctionFCM/app/src/main/java/com/example/functionfcm/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.functionfcm
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivivty"
+
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener{ task ->
+            if(!task.isSuccessful){
+                Log.w(TAG, "onCreate: Fetching FCM registration token failed",task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+            val msg = getString(R.string.msg_token_fmt, token)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 }
---
+++ function/FCM/FunctionFCM/app/src/main/java/com/example/functionfcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,82 @@
+package com.example.functionfcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log.d
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import java.util.logging.Logger
+
+class MyFirebaseMessagingService : FirebaseMessagingService(){
+
+    val TAG = "MyFirebaseMessagingService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            d(TAG, "Message data payload: ${remoteMessage.data}")
+            if(true){
+                scheduleJob()
+            }else{
+                handleNow()
+            }
+        }
+        remoteMessage.notification?.let {
+            d(TAG, "onMessageReceived: ${it.body}")
+        }
+    }
+
+    private fun scheduleJob(){
+        val work = OneTimeWorkRequest.Builder(MainActivity::class.java)
+        d(TAG, "scheduleJob: scheduleJob() 실행")
+    }
+
+    private fun handleNow(){
+        d(TAG, "handleNow: handleNow() 실행")
+    }
+
+    override fun onNewToken(token: String){
+        d(TAG, "onNewToken: Refreshed token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?){
+        d(TAG, "sendRegistrationToServer: sendRegistrationTokenToServer($token)")
+    }
+
+    private fun sendNotification(messageBody: Map<String, String>) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(messageBody["title"])
+            .setContentText(messageBody["body"])
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+}
