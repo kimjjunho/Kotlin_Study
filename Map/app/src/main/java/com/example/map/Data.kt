e142a196fc38634415ab4ff3f873e709fa744ee8
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 14:19:07 +0900

    init::Map

---
+++ Map/app/src/main/java/com/example/map/Data.kt
@@ -0,0 +1,7 @@
+package com.example.map
+
+//위도
+var latitude : Double = 0.0
+
+//경도
+var longitude : Double = 0.0
---
+++ Map/app/src/main/java/com/example/map/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.map
+
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Binder
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.webkit.GeolocationPermissions
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.map.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
+
+    private lateinit var mBinding : ActivityMainBinding
+    private val binding get() = mBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mLocationRequest = LocationRequest.create().apply{
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+
+        checkPermissionForLocation(applicationContext)
+
+        if(checkPermissionForLocation(applicationContext)){
+            startLocationUpdates()
+        }
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.webChromeClient = object : WebChromeClient(){
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String?,
+                callback: GeolocationPermissions.Callback?
+            ) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback)
+                callback?.invoke(origin,true,false)
+            }
+        }
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.swipe.setOnRefreshListener {
+            if(checkPermissionForLocation(applicationContext)){
+                startLocationUpdates()
+            }
+            binding.swipe.isRefreshing = false
+        }
+        binding.webView.addJavascriptInterface(WebAppInterface(latitude, longitude),"getLocation")
+        binding.webView.loadUrl("192.168.253.214:3000")
+
+
+        /*binding.swipe.setOnRefreshListener {
+            if(checkPermissionForLocation(applicationContext)){
+                startLocationUpdates()
+            }
+
+            Toast.makeText(applicationContext, "위도: $latitude" + "경도: $longitude",Toast.LENGTH_SHORT).show()
+            binding.swipe.isRefreshing = false
+        }*/
+
+
+    }
+
+    private fun startLocationUpdates() {
+        //FusedLocationProviderClient의 인스턴스를 생성.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
+        // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    // 시스템으로 부터 위치 정보를 콜백으로 받음
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation)
+        }
+    }
+
+    // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
+    fun onLocationChanged(location: Location) {
+        mLastLocation = location
+
+        latitude = mLastLocation.latitude
+        longitude = mLastLocation.longitude
+    }
+
+
+    // 위치 권한이 있는지 확인하는 메서드
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                // 권한이 없으므로 권한 요청 알림 보내기
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    // 사용자에게 권한 요청 후 결과에 대한 처리 로직
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+
+            } else {
+                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    class WebAppInterface(latitude:Double, longitude:Double){
+        @JavascriptInterface
+        fun getLatitude() = latitude
+
+        @JavascriptInterface
+        fun getLongitude() = longitude
+    }
+}
+
